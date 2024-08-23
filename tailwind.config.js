28bb46315f39dbb8ca4b6d1d542d4fb58cad2ffc
ghazihacker00 <ghazihacker00@gmail.com>
2024-08-24 01:41:45 +0700

    permintaan pak mamay 17

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
+
+// tailwind.config.js
 module.exports = {
+  darkMode: 'class', // Aktifkan mode gelap menggunakan class
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      // Tambahkan ekstensi tema sesuai kebutuhan
+    },
     extend: {},
   },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
   variants: {
     extend: {},
   },
